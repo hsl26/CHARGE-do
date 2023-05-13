412b0dc81ad053781e71e74221de62630eea5654
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 <shuala26@gmail.com>
2023-05-13 21:26:20 +0900

    OPEN API 연결(SERVICE KEY IS NOT REGISTERED ERROR.)

--- chargedo.py
+++ chargedo.py
@@ -1,36 +1,26 @@
 from bs4 import BeautifulSoup as BS 
 import requests
+import sqlite3
+
 import datetime
 
 now = datetime.datetime.now()
-url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst" 
+url = "http://openapi.kepco.co.kr/service/EvInfoServiceV2/getEvSearchList" 
 params = {} # dictionary 자료형
-params['serviceKey'] ="<인증키>"
-#params['numOfRows'] = 50   # (optional) default 10
-#params['pageNo'] = 1   # (optional) default 1
-#params['dataType'] = 'XML'     # (optional) (XML/JSON) default XML
-params['base_date'] = now.strftime("%Y%m%d") 
-# #params['base_time'] = now.strftime("%H%M") 
-params['base_time'] = '0500' # 05시 발표 
-params['nx'] = 60 # 정릉제3동 (격자 X) 
-params['ny'] = 128 # 정릉제3동 (격자 Y)
+params['ServiceKey'] ="3BtB1QOAci15d9JFVNNAEeWHNTZ0Ztykdu7v53YXwDurXZL0zF4uaIwRU9gWQ5Q+aF9Hu5nj/i7Ik0Ywl5QXzQ=="
+params['numOfRows'] = 10   # (optional) default 10
+params['pageNo'] = 1   # (optional) default 1
+# params['addr'] = "전라남도 나주시 빛가람동 120"
 
 res = requests.get(url, params=params)
-#print(res.url) # 전체 url을 string으로 구성하여 전달하는 것도 가능하기는 함. 
-#print(res.text)
+print(res.url) 
+print(res.text)
 
-soup = BS(res.text, 'xml') 
-#print(soup)
+'''
+con = sqlite3.connect('chargedo.db', isolation_level=None)
+cur = con.cursor()
 
-names = []
-for td in soup.select('category'):
-    names.append(td.get_text(strip=True)) 
-#print(names)
+cur.execute('INSERT INTO chargedo (resultCode, resultMsg, numOfRows, pageNo, totalCount, items, addr, chargeTp, cpId, cpNm, cpStat, cpTp, csId, csNm, lat, longi, statUpdateDatetime) VALUES (?, ?)',
+            (1, 3.14))
 
-values = []
-for td in soup.select('fcstValue'):
-    values.append(td.get_text(strip=True)) 
-#print(values)
-
-idx = names.index('TMP') # 1시간 기온 (°C) 
-print(values[idx])
+'''
